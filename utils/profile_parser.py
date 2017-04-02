9675a31f9160a6c210ceaaeb97f9ff348952aef8
Shivam Bansal <shivam5992@gmail.com>
2017-04-02 11:31:04 +0530

    added twitter dataset

---
+++ utils/profile_parser.py
@@ -0,0 +1,20 @@
+companies = open("../data/meta_data/company_list.txt").read().strip().split("\n")
+companies = [company.lower().replace(" ","") for company in companies]
+
+data = open("../data/twitter_data.txt").read().strip().split("\n")
+
+import ast 
+visited = []
+for doc in data:
+	try:
+		doc = ast.literal_eval(doc)
+		name = doc['name'].lower().replace(" ","")
+		desc = doc['description']
+
+		for fil in companies:
+			if fil == name and desc and name not in visited:
+				visited.append(name)
+				print name + "||" + " ".join(desc.split())
+				break
+	except Exception as E:
+		pass 
